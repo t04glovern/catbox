a209f47f3a408a85c834f69edbfe4572f2399815
Rico A. Beti <rico.beti@silentbyte.com>
2018-04-02 03:36:26 +0800

    Add test code for auth

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,8 +1,17 @@
-const functions = require('firebase-functions');
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Firebase Cloud Functions
+//
+
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp(functions.config().firebase);
+
+exports.testFunc = functions.https.onRequest((req, res) => {
+    const tokenId = req.get('Authorization').split('Bearer ')[1];
+
+    return admin.auth().verifyIdToken(tokenId)
+        .then((decoded) => res.status(200).send(decoded))
+        .catch((err) => res.status(401).send(err));
+});
+
