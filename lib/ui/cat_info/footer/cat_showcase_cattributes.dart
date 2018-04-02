021e74ad332847c4b62bf547d75df6426fd90fbd
Nathan Glover <nathan@glovers.id.au>
2018-04-02 23:55:15 +0800

    changed to dynamics instead of strictly typed maps

--- lib/ui/cat_info/footer/cat_showcase_cattributes.dart
+++ lib/ui/cat_info/footer/cat_showcase_cattributes.dart
@@ -3,7 +3,7 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:catbox/ui/cats/cat.dart';
 
-final Map<String, Map<String, dynamic>> cattributeLookup = {
+final dynamic cattributeLookup = {
   "children":     { "name": "Kid Friendly", "icon": FontAwesomeIcons.child      },
   "desexed":      { "name": "Desexed",      "icon": FontAwesomeIcons.cut        },
   "vaccinated":   { "name": "Vaccinated",   "icon": FontAwesomeIcons.eyeDropper },
